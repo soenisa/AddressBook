5643976f932a3286509d6f5e6141524ffa4c1424
Senisa Soenardjo <senisa.s@gmail.com>
2015-07-16 18:07:50 -0400

    Input affiliate field now autocompletes

--- web/touch/touch.js
+++ web/touch/touch.js
@@ -1,4 +1,5 @@
-Contacts = new Mongo.Collection("contacts");
+Contacts = new Mongo.Collection('contacts');
+Affiliates = new Mongo.Collection('affiliates');
 
 if (Meteor.isClient) {
 
@@ -11,6 +12,25 @@ if (Meteor.isClient) {
     }
   });
 
+  Template.addContact.helpers({
+    seshAffiliate: function() {
+      Session.get("inputAffiliates");
+    },
+   settings: function() {
+      return {
+        position: "bottom",
+        limit: 5,
+        rules: [
+          {
+            collection: Affiliates,
+            field: "name",
+            matchAll: false,
+            template: Template.pill
+          }
+        ]
+      };
+    }
+  });
 
   Template.addContact.events({
     "submit form": function (event) {
@@ -18,21 +38,36 @@ if (Meteor.isClient) {
       var fName = event.target.fName.value;
       var lName = event.target.lName.value;
       var num = event.target.num.value;
+      var aff = event.target.aff.value;
 
       Contacts.insert( {
         fname: fName,
         lname: lName,
         num: num,
+        affiliate: aff,
         updated: new Date()
       });
+
+      var affExist = Affiliates.find({name: aff}, {limit: 1}).count();
+
+      if (affExist == 0) {
+        Affiliates.insert( {
+          name: aff
+        });
+      }
 
       event.target.fName.value = "";
       event.target.lName.value = "";
       event.target.num.value = "";
+      event.target.aff.value = "";
     },
     "click .reset": function() {
       Meteor.call("removeAllPosts");
-    }
+    },
+    "autocompleteselect input": function(event, template, doc) {
+      console.log("selected ", doc);
+      Session.set()
+  }
   });
 
   Template.contact.events({
